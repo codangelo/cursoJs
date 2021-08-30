44395ac2c237033a2ffefd2ad5a3bb44f9df75ae
Christian D'Angelo <christiandangelo@gmail.com>
2021-08-30 18:03:24 -0300

    entrega eventos

--- js/app.js
+++ js/app.js
@@ -1,101 +1,149 @@
-// Alta Cliente
-class Cliente {
-  constructor(nombre, apellido, mail) {
-    this.nombre = nombre;
-    this.apellido = apellido;
-    this.mail = mail;
-  }
-  registro() {
-    this.nombre = prompt("Ingresa tu nombre");
-    this.apellido = prompt("Ingresa tu Apellido");
-    this.mail = prompt("Ingresa tu mail");
-
-    console.log(this);
-  }
-  saludo() {
-	const saludo = document.getElementById("saludo")
-	saludo.innerHTML =  `Bienvenido ${this.nombre} tenemos 5 productos disponibles hoy: Lapicera $100; Tijera $399; Goma $50; Calculadora $609 y Hoja $20`
-  }
-}
-
-const nuevoCliente = new Cliente(this.nombre, this.apellido, this.mail);
-
-nuevoCliente.registro();
-nuevoCliente.saludo();
-
-
-
-
-//Lista de productos
-
-class Productos {
-  constructor(id, nombre, precio) {
-    this.id = id;
-    this.nombre = nombre;
-    this.precio = precio;
-  }
-}
-const productos = [
-  { id: 1, nombre: "Lapicera", precio: 100 },
-  { id: 2, nombre: "Tijera", precio: 399 },
-  { id: 3, nombre: "Goma", precio: 50 },
-  { id: 4, nombre: "Calculadora", precio: 609 },
-  { id: 5, nombre: "Hoja", precio: 20 },
+let productos = [
+  {
+    id: 1,
+    nombre: "Master Origins Colombia",
+    descripcion: "AFRUTADO Y VIBRANTE",
+    precio: 1000,
+    img: "../images/col.png"
+  },
+  {
+    id: 2,
+    nombre: "Nicaragua La Cumplida",
+    descripcion: "AFRUTADO Y DULCE",
+    precio: 2000,
+    img: "../images/nic.png"
+  },
+  {
+    id: 3,
+    nombre: "Vanilla Éclair",
+    descripcion: "VANILLA",
+    precio: 1100,
+    img: "../images/vanila.png"
+  },
+  {
+    id: 4,
+    nombre: "Master Origins India",
+    descripcion: "INTENSO Y ESPECIADO",
+    precio: 1500,
+    img: "../images/india.png"
+  },
+  {
+    id: 5,
+    nombre: "Master Origins Indonesia",
+    descripcion: "SABROSO Y AMADERADO",
+    precio: 1500,
+    img: "../images/ind.png"
+  },
+  {
+      id: 6,
+      nombre: "Buenos Aires Lungo",
+      descripcion: "CEREALES Y CARAMELO",
+      precio: 1499,
+      img: "../images/bl.png"
+    },
+    {
+      id: 7,
+      nombre: "Tokyo Vivalto Lungo",
+      descripcion: "FLORAL Y TOSTADO",
+      precio: 1200,
+      img: "../images/tokio.jpg"
+    },
+    {
+      id: 8,
+      nombre: "Vienna Linizio Lungo",
+      descripcion: "MALTEADO, CEREALES",
+      precio: 1350,
+      img: "../images/viena.png"
+    },
+    {
+      id: 9,
+      nombre: "Shanghai Lungo",
+      descripcion: "CÍTRICOS Y AFRUTADO",
+      precio: 1499,
+      img: "../images/shangai.png"
+    },
+    {
+      id: 10,
+      nombre: "Stockholm Fortissio Lungo",
+      descripcion: "TOSTADO Y MALTEADO",
+      precio: 1500,
+      img: "../images/stk.png"
+    },
 ];
 
+const productCard = document.getElementById("contenedor");
 
-//carrito
-class Bag {
-  constructor(producto, precio) {
-    this.producto = producto.toUpperCase();
-    this.precio = parseFloat(precio);
-  }
+productos.forEach((producto) => {
+  const div = document.createElement('div');
+  div.className = "card";
+  div.style = "width: 18rem";
+  div.innerHTML = `
+  <img src=${producto.img} class="card-img-top img" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">${producto.nombre}</h5>
+    <p class="card-text">Precio $${producto.precio}</p>
+    <p class="card-text">${producto.descripcion}</p>
+    <button onclick="agregarAlCarrito(${producto.id})" class="btn btn-primary">Agregar al carrito</button>
+  </div>
+`;
+
+  contenedor.appendChild(div);
+});
+
+const carrito = []
+
+function agregarAlCarrito(prodId){ //recibe el id de producto y lo busca en el array
+  let producto = productos.find ((el) => el.id === prodId)
+  carrito.push(producto)
+  mostrarCarrito()
+}    
+
+
+const tableBody = document.getElementById('tabla-carrito')
+
+const mostrarCarrito = () => {
+
+  tableBody.innerHTML = ""
+
+  carrito.forEach((prod) =>{
+
+  const tr = document.createElement('tr')
+  tr.className = "table-primary"
+  tr.innerHTML = `
+      <th scope="row"><img src=${prod.img} class="fotoCarrito" alt="..."></th>
+      <td>${prod.nombre}</td>
+      <td>${prod.precio}</td>
+      
+  `;
+
+  tableBody.appendChild(tr)
+})
 }
-let bag = [];
-let cantidad = parseInt(prompt("Cuantos productos desea comprar?"));
-for (let i = 1; i <= cantidad; i++) {
-  let producto = prompt("Ingresa el nombre del producto que deseas comprar");
-  if (producto == "Lapicera") {
-    bag.push(new Bag(producto, 100));
-  } else if (producto == "Tijera") {
-    bag.push(new Bag(producto, 399));
-  } else if (producto == "Goma") {
-    bag.push(new Bag(producto, 50));
-  } else if (producto == "Calculadora") {
-    bag.push(new Bag(producto, 609));
-  } else if (producto == "Hoja") {
-    bag.push(new Bag(producto, 20));
-  }
-  console.log(bag);
-}
-const precioTotal = bag.reduce(
-  (acumulador, el) => (acumulador += el.precio),
-  0
-);
-console.log("El total de tu orden es " + precioTotal);
 
-//Calculos compra
-const calculoIva = (x) => x * 0.2;
-const suma = (a, b) => a + b;
-const resta = (a, b) => a - b;
-const dividir = (a, b) => a / b;
+const verCarrito = document.getElementById("verCarrito")
+const tablaCarrito = document.getElementById("tablaCarrito")
+const cerrarCarrito = document.getElementById("cerrarCarrito")
+const vaciarCarrito = document.getElementById("vaciarCarrito")
 
-let precioFinal = suma(precioTotal, calculoIva(precioTotal));
-let envio = 50;
 
-console.log("El precio, IVA incluido es " + precioFinal);
+verCarrito.addEventListener('click',()=>{
+  tablaCarrito.classList.remove("ocultar")
+  cerrarCarrito.classList.remove("ocultar")
+  vaciarCarrito.classList.remove("ocultar")
 
-let precioConEnvio = suma(precioFinal, envio);
+})
 
-alert("El precio final con envío es " + precioConEnvio);
 
-let cuotas = parseInt(prompt("Ingresa la cantidad de cuotas"));
-let pagoCuotas = dividir(precioConEnvio, cuotas);
+vaciarCarrito.addEventListener('click', function(){
+  tableBody.innerHTML = ""
+})
 
-console.log("El valor de la cuota es " + pagoCuotas);
+cerrarCarrito.addEventListener('click',()=>{
+  tablaCarrito.classList.add("ocultar")
+  cerrarCarrito.classList.add("ocultar")
+  vaciarCarrito.classList.add("ocultar")
 
-let cierreOrden =
-  "El total de la orden es " + precioFinal + " IVA incluido. El precio con envío es " + precioConEnvio + ". Si pagas en " + cuotas + " cuota/s, tendrás un costo mensual de " + pagoCuotas;
+})
 
-  const mensaje = document.getElementById("mensaje")
-  mensaje.innerHTML =  cierreOrden
+
+
---
+++ js/app2.js
@@ -0,0 +1,119 @@
+// Alta Cliente
+class Cliente {
+    constructor(nombre, apellido, mail) {
+      this.nombre = nombre;
+      this.apellido = apellido;
+      this.mail = mail;
+    }
+    registro() {
+      this.nombre = prompt("Ingresa tu nombre");
+      this.apellido = prompt("Ingresa tu Apellido");
+      this.mail = prompt("Ingresa tu mail");
+  
+      console.log(this);
+    }
+    saludo() {
+      const saludo = document.getElementById("saludo");
+      saludo.innerHTML = `Bienvenido ${this.nombre} tenemos 5 productos disponibles hoy: Lapicera $100; Tijera $399; Goma $50; Calculadora $609 y Hoja $20`;
+    }
+  }
+  
+  const nuevoCliente = new Cliente(this.nombre, this.apellido, this.mail);
+  
+  nuevoCliente.registro();
+  nuevoCliente.saludo();
+  
+  //Lista de productos
+  
+  const productos = [
+    { id: 1, nombre: "Lapicera", precio: 100 },
+    { id: 2, nombre: "Tijera", precio: 399 },
+    { id: 3, nombre: "Goma", precio: 50 },
+    { id: 4, nombre: "Calculadora", precio: 609 },
+    { id: 5, nombre: "Hoja", precio: 20 },
+  ];
+  
+  const contenedor = document.getElementById("contenedor");
+  
+  publicarProductos(productos);
+  function publicarProductos() {
+    producto.forEach((producto) => {
+      let contProducto = document.createElement("div");
+      contProducto.className = "producto";
+      let nombre = document.createElement("h2");
+      nombre.className = "nombre";
+      marca.textContent = "${productos.nombre";
+      let precio = document.createElement("h3");
+      precio.className = "precio";
+      precio.textContent = "${productos.precio";
+  
+      contProducto.appendChild(nombre);
+      contProducto.appendChild(precio);
+  
+      contenedor.appendChild(contProducto);
+    });
+  }
+  
+  //carrito
+  /* class Bag {
+    constructor(producto, precio) {
+      this.producto = producto.toUpperCase();
+      this.precio = parseFloat(precio);
+    }
+  }
+  let bag = [];
+  let cantidad = parseInt(prompt("Cuantos productos desea comprar?"));
+  for (let i = 1; i <= cantidad; i++) {
+    let producto = prompt("Ingresa el nombre del producto que deseas comprar");
+    if (producto == "Lapicera") {
+      bag.push(new Bag(producto, 100));
+    } else if (producto == "Tijera") {
+      bag.push(new Bag(producto, 399));
+    } else if (producto == "Goma") {
+      bag.push(new Bag(producto, 50));
+    } else if (producto == "Calculadora") {
+      bag.push(new Bag(producto, 609));
+    } else if (producto == "Hoja") {
+      bag.push(new Bag(producto, 20));
+    }
+    console.log(bag);
+  }
+  const precioTotal = bag.reduce(
+    (acumulador, el) => (acumulador += el.precio),
+    0
+  );
+  console.log("El total de tu orden es " + precioTotal);
+  
+  //Calculos compra
+  const calculoIva = (x) => x * 0.2;
+  const suma = (a, b) => a + b;
+  const resta = (a, b) => a - b;
+  const dividir = (a, b) => a / b;
+  
+  let precioFinal = suma(precioTotal, calculoIva(precioTotal));
+  let envio = 50;
+  
+  console.log("El precio, IVA incluido es " + precioFinal);
+  
+  let precioConEnvio = suma(precioFinal, envio);
+  
+  alert("El precio final con envío es " + precioConEnvio);
+  
+  let cuotas = parseInt(prompt("Ingresa la cantidad de cuotas"));
+  let pagoCuotas = dividir(precioConEnvio, cuotas);
+  
+  console.log("El valor de la cuota es " + pagoCuotas);
+  
+  let cierreOrden =
+    "El total de la orden es " +
+    precioFinal +
+    " IVA incluido. El precio con envío es " +
+    precioConEnvio +
+    ". Si pagas en " +
+    cuotas +
+    " cuota/s, tendrás un costo mensual de " +
+    pagoCuotas;
+  
+  const mensaje = document.getElementById("mensaje");
+  mensaje.innerHTML = cierreOrden;
+   */
